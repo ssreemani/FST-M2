e8ac5c64c2c7de3e172cf1e28faa8adda71b4463
ssreemani <52061401+ssreemani@users.noreply.github.com>
2021-07-13 13:34:01 +0530

    Update GitHubProject1.java

--- api-project/GitHubProject1.java
+++ api-project/GitHubProject1.java
@@ -29,13 +29,13 @@ public class GitHubProject1 {
         requestSpec = new RequestSpecBuilder()
                 // Set content type
                 .setContentType(ContentType.JSON)
-                .addHeader("Authorization", "token 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")
+                .addHeader("Authorization", "token <blank>")
                 // Set base URL
                 .setBaseUri("https://api.github.com")
                 // Build request specification
                 .build();
  
-        sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOdlLEZmYtxzTZ6xtyG1Pm07Q11iydLQk9gLrwM7X+bQIkLPGkzzYdubD7v4ZIOQmyc7WekLvLGQxNzvXj34JdEEGGFEcVlJAAuntVPBqe6lP9zeKIFs2L+amYYXpDFo6iXspRX0dwOh+rfKndb12PU9i+cSQqNFd4TSTwVVbfQLHBkqk/PJWFN9SaT/tnXHC6WYBbrahofq04qvkbjvrs8WDuVS9ltf8ai4nNPv7nSNp5pQRF48koEKPgBmUvF8jtUfN/ZEXSqB8+w77N7bJd68+Q4KHwsQ/cRfNfNm2tA8SOhzytq4StkaAiwgLGMgkkPmBVQDzNVaUEXEcvh1tx";
+        sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOdlLEZmYtxzTZ6xtyG1Pm07Q11iydLQk9gLrwM7X+bQIkLPGkzzYdubD7v4ZIOQmyc7WekLvLGQxNzvXj34JdEEGGFEcVlJAAuntVPBqe6lP9zeKIFs2L+amYYXpDFo6iXspRX0dwOh+rfKndb12PU9i+cSQqNFd4TSTwVVbfQLHBkqk/PJWFN9SaT/tnXHC6WYBbrahofq04qvkbjvrs8WDuVS9ltf8ai4nNPv7nSNp5pQRF48koEKPgBmUvF8jtUfN/ZEXSqB8+w77N7bJd68+Q4KHwsQ/cRfNfNm2tA8SOhzytq4StkaAiwgLGMgkkPmBVQDzNVaUEXEcblank";
     }
     
     @Test(priority=1)
